380c85abf4975d4d87bc918844a0261cc231941c
wooram <jibmusae@gmail.com>
2022-10-27 14:40:25 +0900

    Feat: login, logout

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { Users, UsersSchema } from './schemas/users.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './security/passport.jwt.strategy';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
+    JwtModule.register({ secret: process.env.JWT_SECRET_KEY, signOptions: { expiresIn: '300s' } }),
+    PassportModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtStrategy],
   exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,15 @@
 import { Model } from 'mongoose';
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { Users, UsersDocument } from './schemas/users.schema';
 import { UsersDto } from './dto/users.dto';
+import { Payload } from './security/payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(Users.name) private usersModel: Model<UsersDocument>) {}
+  constructor(@InjectModel(Users.name) private usersModel: Model<UsersDocument>, private jwtService: JwtService) {}
 
   // 회원가입
   async register(usersDto: UsersDto): Promise<Users> {
@@ -25,10 +27,29 @@ export class UsersService {
   }
 
   // 로그인
-  async login(usersDto: UsersDto): Promise<void> {
-    // TODO
-    // 1. email, password를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러 처리
-    // 2. JWT를 발급
+  async login(usersDto: UsersDto): Promise<{ accessToken: string } | undefined> {
+    try {
+      const findUser: UsersDto = await this.usersModel.findOne({ email: usersDto.email });
+      const passwordCheck = await bcrypt.compare(usersDto.password, findUser.password);
+
+      if (!findUser || !passwordCheck) {
+        throw new UnauthorizedException();
+      }
+
+      const payload: Payload = {
+        email: findUser.email,
+        role: findUser.role,
+      };
+
+      return { accessToken: this.jwtService.sign(payload) };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // 로그인 토큰 검증
+  async tokenValidateUser(payload: Payload): Promise<UsersDto | undefined> {
+    return await this.usersModel.findOne({ email: payload.email });
   }
 
   // 사용자 목록 조회
@@ -43,14 +64,14 @@ export class UsersService {
   // 사용자 수
   async countUsers(): Promise<number> {
     try {
-      return 0;
+      return await this.usersModel.count();
     } catch (error) {
       console.error(error);
     }
   }
 
   // 사용자 상세 정보
-  async findOne(id: number) {
+  async findOne(id: string): Promise<Users> {
     try {
       return await this.usersModel.findById(id);
     } catch (error) {
@@ -58,13 +79,21 @@ export class UsersService {
     }
   }
 
-  update(id: number, usersUpdateDto) {
-    return `This action updates a #${id} user`;
+  // 사용자 정보 수정
+  async update(id: string, usersDto: UsersDto) {
+    try {
+      return await this.usersModel.updateOne({ _id: id }, { $set: usersDto });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // 회원 탈퇴
+  async remove(id: string) {
+    try {
+      return await this.usersModel.deleteOne({ _id: id });
+    } catch (error) {
+      console.error(error);
+    }
   }
-
-  async;
 }
